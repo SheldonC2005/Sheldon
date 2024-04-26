8a3f5cf32a2f06e6106b5ff1ae95eb08088f83c9
Sheldon <sheldon.c2023@vitstudent.ac.in>
2024-04-26 12:27:28 +0530

    some error....need to verify

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -53,9 +53,9 @@ void board()
     cout<<"Player 1(X) - Player 2(O)"<<endl<<endl;
     cout<<endl;
 
-    cout<<square[1]<<" | "<<square[2]<<" | "<<square[3]<<" | "<<endl;
-    cout<<square[4]<<" | "<<square[5]<<" | "<<square[6]<<" | "<<endl;
-    cout<<square[7]<<" | "<<square[8]<<" | "<<square[9]<<" | "<<endl;
+    cout<<square[1]<<" | "<<square[2]<<" | "<<square[3]<<endl;
+    cout<<square[4]<<" | "<<square[5]<<" | "<<square[6]<<endl;
+    cout<<square[7]<<" | "<<square[8]<<" | "<<square[9]<<endl;
     cout<<endl;
 }
 
@@ -66,7 +66,66 @@ int main()
     do
     {
         board();
-        player
-    } while ();
-    
+        player=(player%2)?1:2;
+        cout<<"Player "<<player<<", enter the number: ";
+        cin>>choice;
+        mark=(player==1)?'X':'O';
+        if(choice==1 && square[1]=='1')
+        {
+            square[1]==mark;
+        }
+        else if(choice==2 && square[2]=='2')
+        {
+            square[2]==mark;
+        }
+        else if(choice==3 && square[3]=='3')
+        {
+            square[3]==mark;
+        }
+        else if(choice==4 && square[4]=='4')
+        {
+            square[4]==mark;
+        }
+        else if(choice==5 && square[5]=='5')
+        {
+            square[5]==mark;
+        }
+        else if(choice==6 && square[6]=='6')
+        {
+            square[6]==mark;
+        }
+        else if(choice==7 && square[7]=='7')
+        {
+            square[7]==mark;
+        }
+        else if(choice==8 && square[8]=='8')
+        {
+            square[8]==mark;
+        }
+        else if(choice==9 && square[9]=='9')
+        {
+            square[9]==mark;
+        }
+        else
+        {
+            cout<<"INVALID MOVE!\n";
+            player--;
+            cin.ignore();
+            cin.get();
+        }
+        i=chechwin();
+        player++;    
+    }while (i==-1);
+    board();
+    if(i==1)
+    {
+        cout<<"\aCONGRATULATIONS!! PLAYER "<<--player<<"WINS!";
+    }
+    else
+    {
+        cout<<"\aGAME DRAW!";
+    }
+    cin.ignore();
+    cin.get();
+    return 0;
 }
