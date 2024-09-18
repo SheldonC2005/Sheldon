4fae49b9a142294ac41765aece72822f4a18d181
Sheldon <sheldon.c2023@vitstudent.ac.in>
2024-09-18 10:11:14 +0530

    Advanced C Lab DA sumbmission for before Lab

--- Advanced C/AdvC ELA DA.c
+++ Advanced C/AdvC ELA DA.c
@@ -127,12 +127,12 @@ int main()
 /*#include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
+#define MAX_n 100
 
 int main()
 {
     int N;
-    printf("Enter the size of the matrix (N x N): ");
+    printf("Enter the n of the matrix (N x N): ");
     scanf("%d", &N);
     int matrix[N][N];
     printf("Enter the elements of the matrix:\n");
@@ -218,11 +218,11 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int stack[size];
+    int n = nof(arr) / nof(arr[0]);
+    int stack[n];
     int top = -1;
     stack[++top] = 0;
-    stack[++top] = size - 1;
+    stack[++top] = n - 1;
     while (top >= 0)
     {
         int high = stack[top--];
@@ -254,7 +254,7 @@ int main()
         }
     }
     printf("Sorted array:\n");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
@@ -331,4 +331,256 @@ int main()
         printf("\n");
    }
    return 0;
+}*/
+
+//                                        ELA DA-3
+//1
+/*#include <stdio.h>
+#define MAX 10
+int isSymmetric(int (*matrix)[MAX], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (matrix[i][j] != matrix[j][i])
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int matrix[MAX][MAX];
+    int size;
+    printf("Enter the size of the matrix (n x n): ");
+    scanf("%d", &size);
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    if (isSymmetric(matrix, size))
+    {
+        printf("The matrix is symmetric.\n");
+    }
+    else
+    {
+        printf("The matrix is not symmetric.\n");
+    }
+    return 0;
+}*/
+//2
+/*#include <stdio.h>
+#include <string.h>
+void cleanString(char *input, char *output)
+{
+    while (*input)
+    {
+        // Check if the character is alphanumeric or a space
+        if ((*input >= 'A' && *input <= 'Z') || (*input >= 'a' && *input <= 'z') || 
+            (*input >= '0' && *input <= '9') || *input == ' ')
+        {
+            *output++ = *input;  // Copy valid characters
+        }
+        input++;
+    }
+    *output = '\0';
+}
+void sortWords(char words[][100], int count)
+{
+    for (int i = 0; i < count - 1; i++)
+    {
+        for (int j = i + 1; j < count; j++)
+        {
+            if (strlen(words[i]) > strlen(words[j]))
+            {
+                char temp[100];
+                strcpy(temp, words[i]);
+                strcpy(words[i], words[j]);
+                strcpy(words[j], temp);
+            }
+        }
+    }
+}
+
+int main()
+{
+    char input[100];
+    char cleaned[100];
+    char words[50][100];
+    int wordCount = 0;
+    printf("Enter a statement: ");
+    fgets(input, 100, stdin);
+    cleanString(input, cleaned);
+    char *token = strtok(cleaned, " ");
+    while (token)
+    {
+        strcpy(words[wordCount++], token);
+        token = strtok(NULL, " ");
+    }
+    sortWords(words, wordCount);
+    for (int i = 0; i < wordCount; i++)
+    {
+        printf("\t%s\n", words[i]);
+    }
+    return 0;
+}*/
+//3
+/*#include <stdio.h>
+union PhoneNumber
+{
+    long long mobile;
+    char landline[100];
+};
+struct GuardianDetails
+{
+    char name[100];
+    char address[100];
+    char relation[100];
+    union PhoneNumber phone;
+    int isMobile;
+};
+void removeNewline(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+    struct GuardianDetails guardian;
+    printf("Enter Guardian Name: ");
+    fgets(guardian.name, 100, stdin);
+    removeNewline(guardian.name);
+    printf("Enter Address: ");
+    fgets(guardian.address, 100, stdin);
+    removeNewline(guardian.address);
+    printf("Enter Relation: ");
+    fgets(guardian.relation, 100, stdin);
+    removeNewline(guardian.relation);
+    printf("Enter Mobile Number (10 digits) or Landline Number: ");
+    char phoneInput[100];
+    fgets(phoneInput, 100, stdin);
+    removeNewline(phoneInput);
+    if (phoneInput[0] >= '0' && phoneInput[0] <= '9' && phoneInput[1] >= '0' && 
+        phoneInput[2] >= '0' && phoneInput[3] >= '0' && phoneInput[4] >= '0' &&
+        phoneInput[5] >= '0' && phoneInput[6] >= '0' && phoneInput[7] >= '0' &&
+        phoneInput[8] >= '0' && phoneInput[9] >= '0')
+    {
+        guardian.phone.mobile = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            guardian.phone.mobile = guardian.phone.mobile * 10 + (phoneInput[i] - '0');
+        }
+        guardian.isMobile = 1;
+    }
+    else
+    {
+        int i = 0;
+        while (phoneInput[i] != '\0' && i < 100 - 1)
+        {
+            guardian.phone.landline[i] = phoneInput[i];
+            i++;
+        }
+        guardian.phone.landline[i] = '\0';
+        guardian.isMobile = 0;
+    }
+    printf("\nGuardian Details:\n");
+    printf("Name: %s\n", guardian.name);
+    printf("Address: %s\n", guardian.address);
+    printf("Relation: %s\n", guardian.relation);
+    if (guardian.isMobile)
+    {
+        printf("Mobile Number: %lld\n", guardian.phone.mobile);
+    } else
+    {
+        printf("Landline Number: %s\n", guardian.phone.landline);
+    }
+    return 0;
+}*/
+//4
+/*#include <stdio.h>
+struct Student
+{
+    char name[50];
+    int reg_number;
+    float cgpa;
+};
+int binarySearch(struct Student students[], int size, int key)
+{
+    int left = 0, right = size - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (students[mid].reg_number == key)
+        {
+            return mid; // Found
+        }
+        if (students[mid].reg_number < key)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+void sortStudents(struct Student students[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (students[i].reg_number > students[j].reg_number)
+            {
+                struct Student temp = students[i];
+                students[i] = students[j];
+                students[j] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    struct Student students[100];
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter name, registration number, and CGPA of student %d:\n", i + 1);
+        scanf("%s %d %f", students[i].name, &students[i].reg_number, &students[i].cgpa);
+    }
+    sortStudents(students, n);
+    int searchRegNumber;
+    printf("Enter registration number to search: ");
+    scanf("%d", &searchRegNumber);
+    int index = binarySearch(students, n, searchRegNumber);
+    if (index != -1)
+    {
+        printf("\nStudent Details:\n");
+        printf("Name: %s\n", students[index].name);
+        printf("Registration Number: %d\n", students[index].reg_number);
+        printf("CGPA: %.2f\n", students[index].cgpa);
+    }
+    else
+    {
+        printf("Student not found.\n");
+    }
+    return 0;
 }*/
